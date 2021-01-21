e618c80fb34973dd990cb3bd44135f82aad8fbd9
pi <pi@vm>
2021-01-20 20:38:35 -0500

    88

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@ LIB=/usr/lib/pmm
 BIN=/usr/bin/pmm
 sudo mkdir -p $LIB
 sudo cp pmm.sh $BIN
-for u in /home/*; do mkdir -p $u/.pmm; done
+for u in /home/*; do
+    uu=`echo $u | awk '{ print gensub(/\/home\//, "", 1) }' -`;
+    mkdir -p $u/.pmm;
+    chown $uu:$uu $u/.pmm
+done
