36c349699e57b884a556eaae9c06c203c9d11fa7
Max Catman <xorgnak@gmail.com>
2021-01-20 21:18:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ LIB=/usr/lib/pmm
 BIN=/usr/bin/pmm
 sudo mkdir -p $LIB
 sudo cp pmm.sh $BIN
-chmod +x $BIN
+sudo chmod +x $BIN
 for u in /home/*; do
     uu=`echo $u | awk '{ print gensub(/\/home\//, "", 1) }' -`;
     mkdir -p $u/.pmm;
